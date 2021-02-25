6c2ac4985d13205704aab5d87b932755a6f14565
lee-zhg <zhangllc@outlook.com>
2021-02-24 21:08:13 -0600

    Update variables.tf

--- roks/variables.tf
+++ roks/variables.tf
@@ -104,6 +104,7 @@ variable "vpc_zone_names" {
 locals {
   max_size        = length(var.vpc_zone_names)
   cluster_name_id = var.on_vpc ? join("", ibm_container_vpc_cluster.cluster.*.id) : join("", ibm_container_cluster.cluster.*.id)
-  roks_version    = format("%s_openshift", split("_", var.roks_version)[0])
+  #roks_version    = format("%s_openshift", split("_", var.roks_version)[0])
+  roks_version    = var.roks_version
 }
 
